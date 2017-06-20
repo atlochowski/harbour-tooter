0580656d1d1d0ca49a7f80ca244121f31094fae4
A <hucul@o2.pl>
2017-06-20 14:57:50 +0200

    Create harbour-tooter-pl.ts

---
+++ translations/harbour-tooter-pl.ts
@@ -0,0 +1,194 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name></name>
+    <message id="Logout">
+        <source></source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="Login">
+        <source></source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>API</name>
+    <message>
+        <source>favourited</source>
+        <translation type="unfinished">favourited</translation>
+    </message>
+    <message>
+        <source>followed you</source>
+        <translation type="unfinished">followed you</translation>
+    </message>
+    <message>
+        <source>boosted</source>
+        <translation type="unfinished">boosted</translation>
+    </message>
+    <message>
+        <source>said</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Browser</name>
+    <message>
+        <source>Open in Browser</source>
+        <translation>Open in Browser</translation>
+    </message>
+    <message>
+        <source>Web mode</source>
+        <translation>Web mode</translation>
+    </message>
+    <message>
+        <source>Reading mode</source>
+        <translation>Reading mode</translation>
+    </message>
+</context>
+<context>
+    <name>Conversation</name>
+    <message>
+        <source>Conversation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Content warning!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>public</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>unlisted</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>followers only</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>direct</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Tooter</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>Home</source>
+        <translation>Home</translation>
+    </message>
+    <message>
+        <source>Timeline</source>
+        <translation>Timeline</translation>
+    </message>
+    <message>
+        <source>Notifications</source>
+        <translation>Notifications</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Search</translation>
+    </message>
+    <message>
+        <source>New Toot</source>
+        <translation>New</translation>
+    </message>
+</context>
+<context>
+    <name>MiniStatus</name>
+    <message>
+        <source>boosted</source>
+        <translation>boosted</translation>
+    </message>
+    <message>
+        <source>favourited</source>
+        <translation>favourited</translation>
+    </message>
+    <message>
+        <source>followed you</source>
+        <translation>followed you</translation>
+    </message>
+</context>
+<context>
+    <name>MyList</name>
+    <message>
+        <source>Load more</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Profile</name>
+    <message>
+        <source>Unfollow</source>
+        <translation>Unfollow</translation>
+    </message>
+    <message>
+        <source>Follow request sent!</source>
+        <translation>Follow request sent!</translation>
+    </message>
+    <message>
+        <source>Following</source>
+        <translation>Following</translation>
+    </message>
+    <message>
+        <source>Mute</source>
+        <translation>Mute</translation>
+    </message>
+    <message>
+        <source>Unmute</source>
+        <translation>Unmute</translation>
+    </message>
+    <message>
+        <source>Unblock</source>
+        <translation>Unblock</translation>
+    </message>
+    <message>
+        <source>Block</source>
+        <translation>Block</translation>
+    </message>
+    <message>
+        <source>Statuses</source>
+        <translation>Statuses</translation>
+    </message>
+    <message>
+        <source>Favourites</source>
+        <translation>Favourites</translation>
+    </message>
+    <message>
+        <source>Follow</source>
+        <translation>Follow</translation>
+    </message>
+    <message>
+        <source>Summary</source>
+        <translation>Summary</translation>
+    </message>
+    <message>
+        <source>Followers</source>
+        <translation>Followers</translation>
+    </message>
+</context>
+<context>
+    <name>Toot</name>
+    <message>
+        <source>boosted</source>
+        <translation>boosted</translation>
+    </message>
+    <message>
+        <source>favourited</source>
+        <translation>favourited</translation>
+    </message>
+    <message>
+        <source>followed you</source>
+        <translation>followed you</translation>
+    </message>
+</context>
+</TS>
